dae4ebdbc2677cc10af9ca1f6bbd2faa0fe927fc
malia.lili <malia.mias@yahoo.fr>
2015-10-01 16:39:07 +0200

    modifié

---
+++ index.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta charset="utf-8" />
+         <meta http-equiv="X-UA-Compatible" content="IE=edge">
+        <title>La liste des ateliers</title>
+         <link rel="stylesheet" href="main.css">
+    </head>
+        
+    <body>
+        <h1>La liste des ateliers </h1>
+
+<?php 
+include("list-atelier.php"); 
+?>
+
+<a href="add.php" class="buttons add" > Ajouter un atelier</a>
+<br>
+
+</body>
+</html>
